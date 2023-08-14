cd0e375a1f6f6e81990d69e21bf031db08cbf385
michaltestka <s21093@pjwstk.edu.pl>
2023-08-14 14:09:55 +0200

    Added readme file and configure proxy

--- ToDoApp/ClientApp/src/setupProxy.js
+++ ToDoApp/ClientApp/src/setupProxy.js
@@ -1,8 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:40962';
+const target = "https://localhost:7281";
 
 const context =  [
   "/todo",
